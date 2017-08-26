2850d5d5378df66ef169cecf0962b955a44d5edf
Jeff Kaleshi <jeff@10-107-210-96.vlan895.client.wireless.uic.edu>
2017-08-26 16:02:58 -0500

    Create a file to start a non production server

---
+++ manage.py
@@ -0,0 +1,13 @@
+# manage.py
+# Written by Jeff Kaleshi
+
+import os
+
+from flask_script import Manager
+from app import create_app
+
+app = create_app(os.environ['ENVIRONMENT'])
+manager = Manager(app)
+
+if __name__ == '__main__':
+    manager.run()
